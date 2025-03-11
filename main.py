bf1fb4cb8e5399827f9e8f968f261682182ced3a
hallma886 <hallma886@northwested.org>
2025-03-11 12:38:00 -0400

    Update Main.py

--- main.py
+++ main.py
@@ -21,20 +21,33 @@ def handle_events():
                 return False  # Return False to indicate quitting
     return True  # Continue running if no quit event
 
+
+
+def draw_text(screen, text, font, text_color, x, y):
+    inserted_text = font.render(text, True, text_color)
+    screen.blit(inserted_text, (x, y))
+
 def main():
 
     screen = init_game()  # Initialize the game and get the screen
     clock = pygame.time.Clock() # Initialize the clock objecct
-    
 
- 
+    text_font = pygame.font.SysFont(None, 30)
     # Main game loop
     running = True
     while running:
         running = handle_events()  # Handle events and check if we should continue running
 
         # Fill the screen with a background color 
-        screen.fill(config.WHITE) 
+        screen.fill(config.PURPLE) 
+
+        
+        draw_text(screen, "Matthew hall", text_font, config.BLUE, 200, 300)
+        draw_text(screen, "Grand Traverse Academy", text_font, config.BLACK, 300, 400)
+
+
+
+
 
         pygame.display.flip()  # Update the display
 
